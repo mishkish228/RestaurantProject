f7223b7c35222753b43fd39eb940000b2108ea3d
mishkish228 <mikhailvasilyevx@gmail.com>
2025-06-12 00:40:46 +0300

    // later commit

---
+++ src/main/java/org/example/restaurantproject/RestaurantProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.restaurantproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantProjectApplication.class, args);
+    }
+
+}
